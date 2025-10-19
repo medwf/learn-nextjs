30268d253f24af68e5c48ac762ba8447dab6b1ff
medwf <med.wf95@gmail.com>
2025-10-19 15:28:58 +0100

    chapter 8: ppr

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,10 +2,9 @@ import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data';
 
 import { Suspense } from 'react';
-import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
     // const revenue = await fetchRevenue();
@@ -25,7 +24,7 @@ export default async function Page() {
                     value={numberOfCustomers}
                     type="customers"
                 /> */}
-                <Suspense fallback={<CardSkeleton />}>
+                <Suspense fallback={<CardsSkeleton />}>
                     <CardWrapper />
                 </Suspense>
             </div>
