175c11c4382c0be6b778617226552be283e6cc39
medwf <med.wf95@gmail.com>
2025-10-24 17:50:54 +0100

    chapter : Improving Accessibility - show error using useActionState with Zod

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,24 +7,45 @@ import { redirect } from "next/navigation";
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
-  id: z.string(),
+  id: z.string({ invalid_type_error: "Please select a customer." }),
   customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(["pending", "paid"]),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Please enter an amount greater than 0$" }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select an invoice status.",
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+export type State = {
+  message: string | null;
+  errors: {
+    customerId?: string[];
+    amount?: string[];
+    state?: string[];
+  };
+};
 
-export async function createInvoice(data: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, data: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: data.get("customerId"),
     amount: data.get("amount"),
     status: data.get("status"),
   });
+  // console.log(validatedFields.error);
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice",
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
-  const date = new Date().toISOString();
+  const date = new Date().toISOString().split("T")[0];
 
   try {
     await sql`
@@ -45,13 +66,25 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 // ...
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice",
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,6 +10,7 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateInvoice } from '@/app/lib/actions';
+import { useActionState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,11 +19,16 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-
+  const initialState = {
+    message: '',
+    errors: {} as { customerId?: string[]; amount?: string[]; status?: string[] },
+  };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+
 
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -34,6 +40,7 @@ export default function EditInvoiceForm({
               id="customer"
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+              aria-describedby='customer-error'
               defaultValue={invoice.customer_id}
             >
               <option value="" disabled>
@@ -46,6 +53,17 @@ export default function EditInvoiceForm({
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id="customer-error" aria-live='polite' aria-atomic='true'>
+            {
+              state.errors?.customerId &&
+              state.errors?.customerId.map((err) => (
+                <p className='mt-2 text-sm text-red-500'>
+                  {err}
+                </p>
+              ))
+
+            }
           </div>
         </div>
 
@@ -63,9 +81,21 @@ export default function EditInvoiceForm({
                 step="0.01"
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
+                aria-describedby='amount-error'
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+            <div id="amount-error" aria-live='polite' aria-atomic='true'>
+              {
+                state.errors?.amount &&
+                state.errors?.amount.map((err) => (
+                  <p className='mt-2 text-sm text-red-500' key={err}>
+                    {err}
+                  </p>
+                ))
+
+              }
             </div>
           </div>
         </div>
@@ -99,6 +129,7 @@ export default function EditInvoiceForm({
                   name="status"
                   type="radio"
                   value="paid"
+                  aria-describedby='status-error'
                   defaultChecked={invoice.status === 'paid'}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
@@ -111,17 +142,38 @@ export default function EditInvoiceForm({
               </div>
             </div>
           </div>
+          <div id="status-error" aria-live='polite' aria-atomic='true'>
+            {
+              state.errors?.status &&
+              state.errors?.status.map((err) => (
+                <p className='mt-2 text-sm text-red-500' key={err}>
+                  {err}
+                </p>
+              ))
+
+            }
+          </div>
+          <div aria-live='polite' aria-atomic='true'>
+            {
+              state.message &&
+              <p className='mt-2 text-sm text-red-500' >
+                {state.message}
+              </p>
+
+            }
+          </div>
         </fieldset>
+
+        <div className="mt-6 flex justify-end gap-4">
+          <Link
+            href="/dashboard/invoices"
+            className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+          >
+            Cancel
+          </Link>
+          <Button type="submit">Edit Invoice</Button>
+        </div>
       </div>
-      <div className="mt-6 flex justify-end gap-4">
-        <Link
-          href="/dashboard/invoices"
-          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
-        >
-          Cancel
-        </Link>
-        <Button type="submit">Edit Invoice</Button>
-      </div>
-    </form>
+    </form >
   );
 }
